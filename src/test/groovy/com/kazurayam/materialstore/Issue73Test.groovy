ffcf5ec190744b8c17fd1292b6aafc02920811ff
kazurayam <kazuaki.urayama@gmail.com>
2022-01-25 18:49:16 +0900

    add com.kazurayam.materialstore.Issue73Test

---
+++ src/test/groovy/com/kazurayam/materialstore/Issue73Test.groovy
@@ -0,0 +1,61 @@
+package com.kazurayam.materialstore
+
+import org.apache.commons.io.FileUtils
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+/**
+ * Reproducing the issue #73 at https://github.com/kazurayam/materialstore/issues/73
+ * and fixing it.
+ */
+class Issue73Test {
+
+    static final Path fixtureDir = Paths.get(".")
+            .resolve("src/test/resources/fixture/issue#73")
+    static final Path outputDir = Paths.get(".")
+            .resolve("build/tmp/testOutput")
+            .resolve("Issue73Test")
+    static Store store
+    static final JobName jobName = new JobName("MyAdmin_visual_inspection_twins")
+    static final JobTimestamp timestampP = new JobTimestamp("20220125_140449")
+    static final JobTimestamp timestampD = new JobTimestamp("20220125_140509")
+
+    MaterialList left
+    MaterialList right
+
+    @BeforeAll
+    static void beforeAll() {
+        if (Files.exists(outputDir)) {
+            outputDir.toFile().deleteDir()
+        }
+        Files.createDirectories(outputDir)
+        Path storePath = outputDir.resolve("store")
+        FileUtils.copyDirectory(fixtureDir.toFile(), storePath.toFile())
+        store = Stores.newInstance(storePath)
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        left = store.select(jobName, timestampP,
+                MetadataPattern.builderWithMap(["profile": "MyAdmin_ProductionEnv"]).build()
+        )
+        assert left.size() == 8
+        right = store.select(jobName, timestampD,
+                MetadataPattern.builderWithMap(["profile": "MyAdmin_DevelopmentEnv"]).build()
+        )
+        assert right.size() == 8
+    }
+
+    @Test
+    void test_smoke() {
+        DiffArtifacts stuffedDiffArtifacts =
+                store.makeDiff(left, right, IgnoringMetadataKeys.of("profile", "URL.host"))
+        int warnings = stuffedDiffArtifacts.countWarnings(0.0d)
+        assert stuffedDiffArtifacts.size() == 8
+    }
+}
