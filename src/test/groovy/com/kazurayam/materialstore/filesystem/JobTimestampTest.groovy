44ca2791499be93529417621f32580fdd131cb5b
kazurayam <kazuaki.urayama@gmail.com>
2022-03-23 15:02:20 +0900

    #64 translated filesystem.JobTimestamp from Groovy to Java

--- src/test/groovy/com/kazurayam/materialstore/filesystem/JobTimestampTest.groovy
+++ src/test/groovy/com/kazurayam/materialstore/filesystem/JobTimestampTest.groovy
@@ -10,7 +10,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals
 import static org.junit.jupiter.api.Assertions.assertTrue
 import static org.junit.jupiter.api.Assertions.assertNotEquals
 
-
 class JobTimestampTest {
 
     private static final Pattern pattern = Pattern.compile("\\d{8}_\\d{6}")
