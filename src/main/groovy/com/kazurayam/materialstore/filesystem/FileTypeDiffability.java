b24fc6ca4bc4801f34a6f1f1d3877dde2421d8b9
kazurayam <kazuaki.urayama@gmail.com>
2022-03-23 09:43:07 +0900

    #64 translated FileTypeDiffability.groovy -> FileTypeDiffability.java

--- src/main/groovy/com/kazurayam/materialstore/filesystem/FileTypeDiffability.java
+++ src/main/groovy/com/kazurayam/materialstore/filesystem/FileTypeDiffability.java
@@ -1,16 +1,12 @@
-package com.kazurayam.materialstore.filesystem
+package com.kazurayam.materialstore.filesystem;
 
 /**
  * Diff-ability
- *
+ * <p>
  * MimeType "text/html" is a simple text so it is diffable by TextDifferToHTML
  * MimeType "image/png" is a image so it is diffable by ImageDifferToPNG
  * MimeType "font/woff2" is a unknown so it is unable to create a diff info.
  */
-enum FileTypeDiffability {
-
-    AS_TEXT,
-    AS_IMAGE,
-    UNABLE;
-
+public enum FileTypeDiffability {
+    AS_TEXT, AS_IMAGE, UNABLE;
 }
