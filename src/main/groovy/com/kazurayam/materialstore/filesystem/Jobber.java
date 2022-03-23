d17c8c64528922f8a82ed76b9a374e8a13b67e47
kazurayam <kazuaki.urayama@gmail.com>
2022-03-23 21:16:08 +0900

    #64 translate filesystem.Jobber from Groovy to Java

---
+++ src/main/groovy/com/kazurayam/materialstore/filesystem/Jobber.java
@@ -0,0 +1,201 @@
+package com.kazurayam.materialstore.filesystem;
+
+import com.kazurayam.materialstore.MaterialstoreException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
+public final class Jobber {
+
+    private static final Logger logger_ = LoggerFactory.getLogger(Jobber.class);
+    private static final String OBJECTS_DIR_NAME = "objects";
+    private final JobName jobName;
+    private final JobTimestamp jobTimestamp;
+    private final Path jobResultDir;
+    private Index index;
+
+    public enum DuplicationHandling {
+        TERMINATE, CONTINUE
+    }
+
+    public Jobber(Path root, JobName jobName, JobTimestamp jobTimestamp) throws IOException, MaterialstoreException {
+        Objects.requireNonNull(root);
+        Objects.requireNonNull(jobName);
+        Objects.requireNonNull(jobTimestamp);
+
+        this.jobName = jobName;
+        this.jobTimestamp = jobTimestamp;
+        jobResultDir = root.resolve(jobName.toString()).resolve(jobTimestamp.toString());
+        Files.createDirectories(getObjectsDir());
+
+        index = new Index();
+        // load content of the "index" file
+        Path indexFile = Index.getIndexFile(jobResultDir);
+        if (Files.exists(indexFile)) {
+            index = Index.deserialize(indexFile);
+        }
+
+    }
+
+    public Path getJobResultDir() {
+        return jobResultDir;
+    }
+
+    public JobName getJobName() {
+        return jobName;
+    }
+
+    public JobTimestamp getJobTimestamp() {
+        return jobTimestamp;
+    }
+
+    public Path getObjectsDir() {
+        return getJobResultDir().resolve(OBJECTS_DIR_NAME);
+    }
+
+    public byte[] read(final ID id, final FileType fileType) {
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(fileType);
+        String fileName = id + "." + fileType.getExtension();
+        Path objectFile = this.getObjectsDir().resolve(fileName);
+        return MaterialIO.deserialize(objectFile);
+    }
+
+    public byte[] read(IndexEntry indexEntry) {
+        Objects.requireNonNull(indexEntry);
+        return this.read(indexEntry.getID(), indexEntry.getFileType());
+    }
+
+    public byte[] read(Material material) {
+        Objects.requireNonNull(material);
+        return this.read(material.getIndexEntry());
+    }
+
+    public MaterialList selectMaterials(final QueryOnMetadata query, final FileType fileType) {
+        Objects.requireNonNull(query);
+        Objects.requireNonNull(fileType);
+        final MaterialList result = new MaterialList(jobName, jobTimestamp, query);
+        for (Iterator<IndexEntry> iter = index.iterator(); iter.hasNext(); ) {
+            IndexEntry indexEntry = iter.next();
+            if (query.equals(QueryOnMetadata.ANY) || query.matches(indexEntry.getMetadata())) {
+                if (fileType.equals(FileType.NULL_OBJECT) || fileType.equals(indexEntry.getFileType())) {
+                    Material material = new Material(getJobName(), getJobTimestamp(), indexEntry);
+                    result.add(material);
+                }
+            }
+        }
+        return result;
+    }
+
+    public MaterialList selectMaterials(QueryOnMetadata query) {
+        return selectMaterials(query, FileType.NULL_OBJECT);
+    }
+
+    /**
+     *
+     */
+    public Material selectMaterial(final ID id) {
+        Objects.requireNonNull(id);
+        for (Iterator<IndexEntry> iter = index.iterator(); iter.hasNext(); ) {
+            IndexEntry indexEntry = iter.next();
+            if (indexEntry.getMaterialIO().getID().equals(id)) {
+                return new Material(jobName, jobTimestamp, indexEntry);
+            }
+        }
+        return Material.NULL_OBJECT;
+    }
+
+    public int size() {
+        return index.size();
+    }
+
+    /**
+     * This method writes the "byte[] data" into a File on disk.
+     * The path of the file will be
+     * <root>/<JobName>/<JobTimestamp>/objects/<sha1 hash id>.<FileType.extension>
+     * <p>
+     * And the "index" file will record MaterialIO objects; 1 line per 1 single MaterialIO.
+     * An entry of "index" will be like:
+     * <sha1 hash id>¥t<FileType.extension>¥t<Metadata>
+     * <p>
+     * The "index" entries are identified uniquely by the combination of
+     * <FileType.extension> + <Metadata>
+     * <p>
+     * You can not write (create) 2 or more IndexEntry objects with the same
+     * <FileType.extension> + <Metadata> combination.
+     * <p>
+     * If you try to do write it, a MaterialstoreException will be raised.
+     * <p>
+     * However, you can create 2 or more IndexEntry objects
+     * with different key with the same `<sha1 hash id>.<FileType.extension>`.
+     * <p>
+     * This means, you would possibly see such an index entries:
+     *
+     * <pre>
+     * ac9be9a1053828f1e12e1cee4d66ff66adf60f9f	png	{"URL.file":"/", profile":"DevelopmentEnv"}
+     * ac9be9a1053828f1e12e1cee4d66ff66adf60f9f	png	{"URL.file":"/", profile":"ProductionEnv"}
+     * </pre>
+     *
+     *
+     */
+    public Material write(byte[] data, FileType fileType, Metadata metadata) throws MaterialstoreException {
+        return write(data, fileType, metadata, DuplicationHandling.TERMINATE);
+    }
+
+    public Material write(byte[] data, final FileType fileType, final Metadata metadata,
+                          final DuplicationHandling duplicationHandling)
+            throws MaterialstoreException {
+        Objects.requireNonNull(metadata);
+        if (data.length == 0) throw new IllegalArgumentException("length of the data is 0");
+        Objects.requireNonNull(fileType);
+
+        if (index.containsKey(fileType, metadata)) {
+            // the metadata has already been put in the index
+            String msg1 = "The combination of " + "fileType=" + fileType.getExtension() + " and metadata=" + metadata + " is already there in the index";
+            if (duplicationHandling.equals(DuplicationHandling.TERMINATE)) {
+                // will stop the process entirely
+                throw new MaterialstoreException(msg1 + ".");
+
+            } else if (duplicationHandling.equals(DuplicationHandling.CONTINUE)) {
+                logger_.info(msg1 + "; process skips one write and continue ...");
+                // look up an entry of the index
+                List<IndexEntry> indexEntries = index.indexEntriesOf(fileType, metadata);
+                assert indexEntries.size() > 0;
+                // return the Material
+                return new Material(this.getJobName(), this.getJobTimestamp(), indexEntries.get(0));
+
+            } else {
+                throw new RuntimeException("Unsupported DuplicationHandling " + duplicationHandling);
+            }
+
+        } else {
+            // new metadata should be stored in the directory
+            // write the byte[] data into file if the MaterialIO is not yet there.
+            ID id = new ID(MaterialIO.hashJDK(data));
+            MaterialIO mio = new MaterialIO(id, fileType);
+            if (!mio.existsInDir(this.getObjectsDir())) {
+                // save the "byte[] data" into disk
+                Path objectFile = this.getObjectsDir().resolve(mio.getFileName());
+                MaterialIO.serialize(data, objectFile);
+            }
+
+            // insert a line into the "index" content on memory
+            IndexEntry indexEntry = index.put(mio.getID(), fileType, metadata);
+            // save the content of the "index" into a file on disk
+            index.serialize(Index.getIndexFile(jobResultDir));
+            return new Material(this.getJobName(), this.getJobTimestamp(), indexEntry);
+        }
+
+    }
+
+    public static String getOBJECTS_DIR_NAME() {
+        return OBJECTS_DIR_NAME;
+    }
+
+}
