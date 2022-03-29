a5167574de51313eaa7c77ee0c6867ca36669a91
kazurayam <kazuaki.urayama@gmail.com>
2022-03-29 11:31:31 +0900

    #64 translated filesystem/metadata/SemanticVersionPattern.groovy to SemanticVersionPattern.java; translated filesystem/metadata/SemanticVersionMatcherResult.groovy to SemanticVersionMatcherResult.java

---
+++ src/main/groovy/com/kazurayam/materialstore/filesystem/metadata/SemanticVersionMatcherResult.java
@@ -0,0 +1,84 @@
+package com.kazurayam.materialstore.filesystem.metadata;
+
+import com.kazurayam.materialstore.filesystem.Jsonifiable;
+import com.kazurayam.materialstore.util.JsonUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+
+public class SemanticVersionMatcherResult implements Jsonifiable {
+
+    private boolean matched;
+    private List<String> fragments;
+
+    public SemanticVersionMatcherResult(Matcher m) {
+        this.matched = m.matches();
+        fragments = new ArrayList<String>();
+        if (m.matches()) {
+            for (int i = 1; i <= m.groupCount() ; i++){
+                fragments.add(m.group(i));
+            }
+        }
+    }
+
+    public boolean matched() {
+        return matched;
+    }
+
+    public List<String> fragments() {
+        return fragments;
+    }
+
+    public int size() {
+        return fragments.size();
+    }
+
+    public String getHeader() {
+        return fragments.get(0);
+    }
+
+    public String getTrailer() {
+        return fragments.get(3);
+    }
+
+    public String getVersion() {
+        return fragments.get(1);
+    }
+
+    @Override
+    public String toJson() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        sb.append("\"matched\":");
+        sb.append(matched);
+        sb.append(",");
+        sb.append("\"fragments\":");
+        sb.append("[");
+        int count = 0;
+        for (String fragment : fragments) {
+            if (count > 0) {
+                sb.append(", ");
+            }
+            sb.append("\"");
+            if (fragment != null) {
+                sb.append(JsonUtil.escapeAsJsonString(fragment));
+            }
+            sb.append("\"");
+            count += 1;
+        }
+        sb.append("]");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    @Override
+    public String toJson(boolean prettyPrint) {
+        if (prettyPrint) {
+            return JsonUtil.prettyPrint(toJson());
+        } else {
+            return toJson();
+        }
+    }
+
+}
