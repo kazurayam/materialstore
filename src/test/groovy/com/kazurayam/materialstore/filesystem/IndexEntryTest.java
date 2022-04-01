9564b37e0da94da88b91ad51a1efa3d41c0f369e
kazurayam <kazuaki.urayama@gmail.com>
2022-04-01 20:46:28 +0900

    #64 translate filesystem/IndexEntryTest.groovy to IndexEntryTest.java

---
+++ src/test/groovy/com/kazurayam/materialstore/filesystem/IndexEntryTest.java
@@ -0,0 +1,44 @@
+package com.kazurayam.materialstore.filesystem;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import groovy.json.JsonOutput;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class IndexEntryTest {
+
+    private final String sampleLine = "6141b40cfe9e7340a483a3097c4f6ff5d20e04ea\tpng\t{\"URL\":\"http://demoaut-mimic.kazurayam.com/\", \"profile\":\"DevelopmentEnv\"}";
+
+    @Test
+    public void test_parseLine() {
+        IndexEntry indexEntry = IndexEntry.parseLine(sampleLine);
+        Assertions.assertEquals("6141b40", indexEntry.getShortId());
+        Assertions.assertNotNull(indexEntry);
+    }
+
+    @Test
+    public void test_toString() {
+        IndexEntry indexEntry = IndexEntry.parseLine(sampleLine);
+        String s = indexEntry.toString();
+        DefaultGroovyMethods.println(this, JsonOutput.prettyPrint(s));
+        assert !s.contains("\"{\"FileType");
+        assert !s.contains("\"{\"URL");
+    }
+
+    @Test public void test_forTemplate () {
+        IndexEntry indexEntry = IndexEntry.parseLine(sampleLine);
+        Map<String, Object> map = indexEntry.toTemplateModel();
+        // print map keys and values
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(map));
+        //
+        Assertions.assertTrue(((String) map.get("id")).startsWith("6141"));
+        Assertions.assertTrue(map.get("fileType") instanceof Map);
+        Assertions.assertTrue(map.get("metadata") instanceof Map);
+    }
+
+}
