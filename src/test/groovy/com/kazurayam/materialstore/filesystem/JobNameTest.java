f99bf6d767a1f0a9eabe413f9d4e36b81348cd28
kazurayam <kazuaki.urayama@gmail.com>
2022-04-02 10:41:30 +0900

    #64 translated filesystem/JobNameTest.groovy to JobNameTest.java

---
+++ src/test/groovy/com/kazurayam/materialstore/filesystem/JobNameTest.java
@@ -0,0 +1,27 @@
+package com.kazurayam.materialstore.filesystem;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class JobNameTest {
+    /**
+     * isValid(String) throws an IllegalArgumentException when the argument String contains
+     * a character listed in the com.kazurayam.materialstore.FileName.PROHIBITED_CHARACTERS.
+     * For example, '/' is prohibited.
+     */
+    @Test
+    public void test_isValid() {
+        Exception e = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            JobName.isValid("/");
+        });
+        Assertions.assertTrue(e.getMessage().contains("/") && e.getMessage().contains("prohibited"));
+    }
+
+    @Test
+    public void test_toString() {
+        Assertions.assertEquals("foo", new JobName("foo").toString());
+    }
+
+}
