ab222141337c93e9ade281ea63c2a0f6eede68c4
kazurayam <kazuaki.urayama@gmail.com>
2022-04-30 23:03:29 +0900

    add com.kazurayam.materialstore.dot.MPGVisualizer class

---
+++ src/main/java/com/kazurayam/materialstore/dot/MPGVisualizer.java
@@ -0,0 +1,64 @@
+package com.kazurayam.materialstore.dot;
+
+import com.kazurayam.materialstore.MaterialstoreException;
+import com.kazurayam.materialstore.filesystem.FileType;
+import com.kazurayam.materialstore.filesystem.JobName;
+import com.kazurayam.materialstore.filesystem.JobTimestamp;
+import com.kazurayam.materialstore.filesystem.Metadata;
+import com.kazurayam.materialstore.filesystem.Store;
+import com.kazurayam.materialstore.reduce.MProductGroup;
+
+import java.awt.image.BufferedImage;
+import java.nio.file.Path;
+import java.util.Collections;
+
+public class MPGVisualizer {
+
+    private final Store store;
+
+    public MPGVisualizer(Store store) {
+        this.store = store;
+    }
+
+    public void visualize(JobName jobName,
+                          JobTimestamp jobTimestamp,
+                          MProductGroup mProductGroup)
+            throws MaterialstoreException {
+        //
+        String dotBeforeZip = DotGenerator.generateDotOfMPGBeforeZip(
+                mProductGroup, Collections.emptyMap(), true);
+        BufferedImage imgBeforeZip = DotGenerator.toImage(dotBeforeZip);
+
+        store.write(jobName, jobTimestamp, FileType.DOT,
+                Metadata.builder()
+                        .put("description",
+                                "DOT text of MProductGroup object BEFORE Zipping 2 MaterialList objects")
+                        .build(),
+                dotBeforeZip);
+
+        store.write(jobName, jobTimestamp, FileType.PNG,
+                Metadata.builder()
+                        .put("description",
+                                "Diagram of MProductGroup object BEFORE Zipping 2 MaterialList objects")
+                        .build(),
+                imgBeforeZip);
+        //
+        String dotAfterZip = DotGenerator.generateDot(
+                mProductGroup, Collections.emptyMap(), true);
+        BufferedImage imgAfterZip = DotGenerator.toImage(dotAfterZip);
+
+        store.write(jobName, jobTimestamp, FileType.DOT,
+                Metadata.builder()
+                        .put("description",
+                                "DOT text of MProductGroup object AFTER constructing List<MaterialProduct>")
+                        .build(),
+                dotAfterZip);
+
+        store.write(jobName, jobTimestamp, FileType.PNG,
+                Metadata.builder()
+                        .put("description",
+                                "Diagram of MProductGroup object AFTER constructing List<MaterialProduct>")
+                        .build(),
+                imgAfterZip);
+    }
+}
