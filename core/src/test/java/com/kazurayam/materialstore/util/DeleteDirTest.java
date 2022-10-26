ddba614d8aab308505baa6ce3148697af4c76933
kazurayam <kazuaki.urayama@gmail.com>
2022-10-26 09:54:09 +0900

    #335

---
+++ core/src/test/java/com/kazurayam/materialstore/util/DeleteDirTest.java
@@ -0,0 +1,33 @@
+package com.kazurayam.materialstore.util;
+
+import com.kazurayam.materialstore.TestCaseSupport;
+import com.kazurayam.materialstore.TestHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DeleteDirTest {
+
+    private TestCaseSupport tcSupport;
+
+    @BeforeEach
+    public void beforeEach() throws IOException {
+        tcSupport = new TestCaseSupport(this);
+    }
+
+    @Test
+    public void test_copyDirectory_overwriting() throws IOException {
+        Path sourceDir = TestHelper.getFixturesDirectory().resolve("issue#331");
+        Path targetDir = tcSupport.getOutputDir();
+        Files.walkFileTree(sourceDir, new CopyDir(sourceDir, targetDir));
+        assertTrue(Files.exists(targetDir));
+        DeleteDir.deleteDirectoryRecursively(targetDir);
+        assertFalse(Files.exists(targetDir));
+    }
+}
