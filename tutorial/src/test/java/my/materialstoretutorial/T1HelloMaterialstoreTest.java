08992f152d9ae6ecde793f8064d7c8b194bedc3e
kazurayam <kazuaki.urayama@gmail.com>
2022-11-27 10:52:58 +0900

    #214 started with the subproject :tutorial and writing 'Materialstore Tutorial' document

---
+++ tutorial/src/test/java/my/materialstoretutorial/T1HelloMaterialstoreTest.java
@@ -0,0 +1,76 @@
+package my.materialstoretutorial;
+
+import com.kazurayam.materialstore.core.filesystem.FileType;
+import com.kazurayam.materialstore.core.filesystem.JobName;
+import com.kazurayam.materialstore.core.filesystem.JobTimestamp;
+import com.kazurayam.materialstore.core.filesystem.Material;
+import com.kazurayam.materialstore.core.filesystem.MaterialstoreException;
+import com.kazurayam.materialstore.core.filesystem.Metadata;
+import com.kazurayam.materialstore.core.filesystem.Store;
+import com.kazurayam.materialstore.core.filesystem.Stores;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/*
+ * This code demonstrate how to save a text string into an instance of
+ * "materialstore" backed with a directory on the local OS file system.
+ */
+public class T1HelloMaterialstoreTest {
+
+    // central abstraction of Material storage
+    private Store store;
+
+    @BeforeEach
+    public void beforeEach() throws IOException {
+        // create a base directory
+        Path dir = createTestClassOutputDir(this);   // (1)
+        // create a directory named "store"
+        Path storeDir = dir.resolve("store");   // (2)
+        // instantiate a Store object
+        store = Stores.newInstance(storeDir);        // (3)
+    }
+
+    /**
+     *
+     * @throws MaterialstoreException
+     */
+    @Test
+    public void test01_hello_materialstore() throws MaterialstoreException {
+        JobName jobName =
+                new JobName("test01_hello_materialstore");       // (4)
+        JobTimestamp jobTimestamp = JobTimestamp.now();          // (5)
+        Material material = store.write(jobName, jobTimestamp,   // (6)
+                FileType.TXT,                            // (7)
+                Metadata.NULL_OBJECT,                    // (8)
+                "Hello, materialstore!");                // (9)
+        assertNotNull(material);
+    }
+
+    //-----------------------------------------------------------------
+
+    Path createTestClassOutputDir(Object testClass) {
+        Path output = getTestOutputDir()
+                .resolve(testClass.getClass().getName());
+        try {
+            if (!Files.exists(output)) {
+                Files.createDirectories(output);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return output;
+    }
+
+    Path getTestOutputDir() {
+        return Paths.get(System.getProperty("user.dir"))
+                .resolve("build/tmp/testOutput");
+    }
+}
+
